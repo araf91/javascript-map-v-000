14b7a99df9ad51db566b91ea87146e514f0ac416
araf91 <araf91@gmail.com>
2017-02-20 20:17:39 -0500

    Done.

--- test/index-test.js
+++ test/index-test.js
@@ -1,55 +1,18 @@
-function map(collection, callback) {
-  const result = [];
+const expect = require('expect')
 
-  for (let i = 0; i < collection.length; i++) {
-    const element = collection[i];
-    result.push(callback(element));
-  }
+const fs = require('fs')
+const jsdom = require('mocha-jsdom')
+const path = require('path')
 
-  return result;
-}
 
-const numbers = [1, 2, 3];
-const doubledNumbers = map(numbers, function (number) {
-  return number * 2;
-});
-console.log(doubledNumbers); // prints [2, 4, 6]
+describe('index', () => {
 
-const autobots = [
-  { name: 'Optimus Prime', strength: 5, isTransformed: false, },
-  { name: 'Ironhide', strength: 3, isTransformed: false, },
-  { name: 'Bumblebee', strength: 2.5, isTransformed: false, },
-  { name: 'Ratchet', strength: 1.5, isTransformed: false, },
-];
+  jsdom({
+    src: fs.readFileSync(path.resolve(__dirname, '..', 'index.js'), 'utf-8')
+  })
 
-const transformedAutobots = map(autobots, function (autobot) {
-  return Object.assign({}, autobot, {
-    strength: autobot.strength * 2,
-    isTransformed: true,
-  });
-});
 
-console.log(transformedAutobots);
-/**
- Prints:
- [ { name: 'Optimus Prime', strength: 10, isTransformed: true },
- { name: 'Ironhide', strength: 6, isTransformed: true },
- { name: 'Bumblebee', strength: 5, isTransformed: true },
- { name: 'Ratchet', strength: 3, isTransformed: true } ]
- */
-
-const transformedAutobotsWithMap = autobots.map(function (autobot) {
-  return Object.assign({}, autobot, {
-    strength: autobot.strength * 2,
-    isTransformed: true,
-  });
-});
-
-console.log(transformedAutobotsWithMap);
-/**
- Prints:
- [ { name: 'Optimus Prime', strength: 10, isTransformed: true },
- { name: 'Ironhide', strength: 6, isTransformed: true },
- { name: 'Bumblebee', strength: 5, isTransformed: true },
- { name: 'Ratchet', strength: 3, isTransformed: true } ]
- */
+  it('runs', () => {
+    expect(true).to.be.true
+  })
+})
